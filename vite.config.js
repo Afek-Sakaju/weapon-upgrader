3cd04f7e0aab74fe8b844d7a87e09b6802c32c8f
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-11 22:35:52 +0300

    init vite app

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig(() => ({
+  plugins: [react()],
+  base: "weapon-upgrade-form",
+  esbuild: {
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+        ".ts": "tsx",
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: "@src", replacement: path.resolve(__dirname, "./src") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "@base-components",
+        replacement: path.resolve(__dirname, "./src/base-components"),
+      },
+      {
+        find: "@utils",
+        replacement: path.resolve(__dirname, "./src/utils"),
+      },
+      {
+        find: "@store",
+        replacement: path.resolve(__dirname, "./src/store"),
+      },
+    ],
+  },
+  server: {
+    open: true,
+  },
+}));
